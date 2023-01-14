6d9b3d122bab78322f304c65f6be7c90d2b635cf
codeaid-dev <codeaid.lab@gmail.com>
2023-01-14 15:18:41 +0900

    fix

--- concentration.py
+++ concentration.py
@@ -3,30 +3,69 @@ import random
 
 side = 125
 one, two = None, None
-count = 0
-stat = 0
+timer = 0
+proc = 0
+stats = [0]*16
 ids = []
 colors = ['red','green','blue','yellow','magenta','cyan','purple','orange']
 tiles = []
-while len(tiles) < 16:
-    c = random.choice(colors)
-    if tiles.count(c) < 2:
-        tiles.append(c)
+
+def draw():
+    cvs.delete('all')
+    for i in range(16):
+        x1 = i%4*side
+        y1 = i//4*side
+        x2 = i%4*side+side
+        y2 = i//4*side+side
+        if stats[i] == 0:
+            cvs.create_rectangle(x1,y1,x2,y2,fill='black',outline='white')
+        if stats[i] == 1 or stats[i] == 2:
+            cvs.create_rectangle(x1,y1,x2,y2,fill=tiles[i],outline='white')
+
+def suffle():
+    tiles.clear()
+    while len(tiles) < 16:
+        c = random.choice(colors)
+        if tiles.count(c) < 2:
+            tiles.append(c)
 
 def click(e):
-    global stat, count
-    for i in range(16):
-        if i%4*side<e.x<i%4*side+side and i//4*side<e.y<i//4*side+side:
-            cvs.itemconfig(ids[i], fill=tiles[i])
-            if one == None:
+    global proc, timer, one, two
+    x = e.x//side
+    y = e.y//side
+    i = x+y*4
+    if 0<=i<=15:
+        if stats[i] == 0:
+            if proc == 1:
+                stats[i] = 1
                 one = i
-                break
-            elif two == None:
+                proc = 2
+            elif proc == 2:
+                stats[i] = 1
                 two = i
-            elif one != None and two != None:
-                if one != two:
-                    cvs.itemconfig(ids[i], fill='black')
+                proc = 3
+                timer = 0
 
+def main():
+    global proc, timer
+    timer += 1
+    draw()
+    if proc == 0:
+        suffle()
+        proc = 1
+#    if proc == 1:
+#        print('1枚目')
+#    if proc == 2:
+#        print('2枚目')
+    if proc == 3 and timer == 5:
+        if tiles[one] == tiles[two]:
+            stats[one] = 2
+            stats[two] = 2
+        else:
+            stats[one] = 0
+            stats[two] = 0
+        proc = 1
+    root.after(200,main)
 
 root = tkinter.Tk()
 root.title('ミニ神経衰弱')
@@ -36,8 +75,5 @@ root.bind('<Button>',click)
 cvs = tkinter.Canvas(root,width=500,height=500,bg='white')
 cvs.pack()
 
-for i in range(16):
-    id = cvs.create_rectangle(i%4*side,i//4*side,i%4*side+side,i//4*side+side,fill='black',outline='white')
-    ids.append(id)
-
+main()
 root.mainloop()
