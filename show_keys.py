c4ff5cee155affeaa805df2840b27e136484687a
codeaid-dev <codeaid.lab@gmail.com>
2023-01-16 14:47:27 +0900

    add

---
+++ show_keys.py
@@ -0,0 +1,15 @@
+import tkinter
+
+def key(e):
+    cvs.delete('all')
+    cvs.create_text(200,50,text=f'keycode = {e.keycode}',fill='black',font=('メイリオ',28))
+    cvs.create_text(200,150,text=f'keysym = {e.keysym}',fill='black',font=('メイリオ',28))
+
+root = tkinter.Tk()
+root.title('キー表示')
+root.bind('<Key>', key)
+cvs = tkinter.Canvas(root, width=400, height=200, bg='white')
+cvs.pack()
+cvs.create_text(200,100,text='何かキーを押してください。',fill='black',font=('メイリオ',28))
+
+root.mainloop()
