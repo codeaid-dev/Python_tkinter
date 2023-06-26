98654a08965425657d98e52e2ffab1c7fe0f6e0a
codeaid-dev <codeaid.lab@gmail.com>
2023-06-26 13:53:55 +0900

    add

---
+++ circle_fall.py
@@ -0,0 +1,40 @@
+import tkinter
+import random
+
+class Circle:
+    def __init__(self):
+        self.size = random.randint(5,50)
+        self.x = random.randint(0,500-self.size)
+        self.y = random.randint(0,500-self.size)
+        self.speedy = random.randint(1,6)
+
+circles = []
+
+def move():
+    for en in circles:
+        if en.y > 500:
+            en.size = random.randint(5,50)
+            en.x = random.randint(0,500-en.size)
+            en.y = -en.size
+            en.speedy = random.randint(1,6)
+        en.y += en.speedy
+        cvs.coords(en.id,en.x,en.y,
+                   en.x+en.size,en.y+en.size)
+    root.after(10, move)
+
+root = tkinter.Tk()
+root.title('円が落ちる')
+cvs = tkinter.Canvas(root, width=500,
+                    height=500,
+                    bg='white')
+cvs.pack()
+for i in range(50):
+    en = Circle()
+    en.id = cvs.create_oval(en.x,en.y,
+                en.x+en.size,en.y+en.size,
+                fill='black',
+                width=0)
+    circles.append(en)
+
+move()
+root.mainloop()
