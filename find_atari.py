7184ffee26a1fd88a966395cbe713f2b691bd034
codeaid-dev <codeaid.lab@gmail.com>
2023-07-01 09:59:31 +0900

    add

---
+++ find_atari.py
@@ -0,0 +1,26 @@
+import tkinter, random
+
+bs = 100
+by = (200-bs)/2
+space = (900-bs*5)//6
+atari = random.randint(0,4)
+
+def pressed(e):
+    for i in range(5):
+        if i*(bs+space)+space<e.x<i*(bs+space)+space+bs and by<e.y<by+bs:
+            if i == atari:
+                cvs.create_oval(i*(bs+space)+space,by,
+                        i*(bs+space)+space+bs,by+bs,
+                        fill='red',width=0)
+
+root = tkinter.Tk()
+root.title('当たりを探せ')
+root.bind('<Button>', pressed)
+cvs = tkinter.Canvas(root, width=900, height=200, bg='white')
+cvs.pack()
+for i in range(5):
+    cvs.create_rectangle(i*(bs+space)+space,by,
+                i*(bs+space)+space+bs,by+bs,
+                fill='black',width=0)
+
+root.mainloop()
