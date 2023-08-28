bdcead5dcef52471a02057a913829ee5ca560724
codeaid-dev <codeaid.lab@gmail.com>
2023-08-28 11:25:40 +0900

    fix

--- circle_click.py
+++ circle_click.py
@@ -13,7 +13,7 @@ circles = []
 
 def pressed(event):
     for en in circles:
-        dist = (abs(en.x+25-event.x)**2+abs(en.y+25-event.y)**2)**0.5
+        dist = ((en.x+25-event.x)**2+(en.y+25-event.y)**2)**0.5
         if dist < 25:
             en.stat = False
 
