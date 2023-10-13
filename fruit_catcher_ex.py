0ae0502880c6477ee4895d3a5b7f3c0666f095cb
codeaid-dev <codeaid.lab@gmail.com>
2023-10-13 14:06:13 +0900

    fix

--- fruit_catcher_ex.py
+++ fruit_catcher_ex.py
@@ -32,18 +32,18 @@ def main():
     fy += fs
     if fy > 500:
         fx = random.randint(25,475)
-        fy = -30
+        fy = -25
         fs = random.randint(3,10)
     cvs.coords(fruit,fx,fy)
     cvs.coords(kago,px,py)
     if fx+25>=px-25 and fx-25<=px+25 and fy+25>=py-9 and fy-25<=py+9:
         score += 1
         fx = random.randint(25,475)
-        fy = -30
+        fy = -25
         fs = random.randint(3,10)
     cvs.delete('score')
     cvs.create_text(250,250,text=f'score: {score}',font=('メイリオ',28),tag='score',fill='black')
-    if timer >= 10000:
+    if timer >= 3000:
         return
     root.after(10,main)
 
@@ -54,9 +54,9 @@ root.bind('<KeyRelease>', key_up)
 cvs = tkinter.Canvas(root, width=500, height=500, bg='white')
 cvs.pack()
 
-fruit_img = tkinter.PhotoImage(file=f'images/fruit_ringo_50.png')
+fruit_img = tkinter.PhotoImage(file='images/fruit_ringo_50.png')
 fruit = cvs.create_image(fx,fy,image=fruit_img)
-kago_img = tkinter.PhotoImage(file=f'images/fruit_kago_50.png')
+kago_img = tkinter.PhotoImage(file='images/fruit_kago_50.png')
 kago = cvs.create_image(px,py,image=kago_img)
 move()
 main()
