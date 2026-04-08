2bcb179912e4d0cc4e5bbc062467c613dc034b35
codeaid-dev <codeaid.lab@gmail.com>
2026-04-08 22:32:18 +0900

    add fix

--- mathematics/round4_1.py
+++ mathematics/round4_1.py
@@ -1,24 +1,18 @@
 import tkinter,math
 
 dir=0
-radius=200
+rw, rh = 200, 50 # rw:横半径、rh:縦半径
 centerX, centerY = 250, 250
 def main():
     global dir
     cvs.delete('circle')
-    x = centerX+radius*math.cos(dir*math.pi/180)
-    y = centerY+radius*math.sin(dir*math.pi/180)
+    x = centerX+rw*math.cos(dir*math.pi/180)
+    y = centerY+rh*math.sin(dir*math.pi/180)
     cvs.create_oval(x-10,y-10,
                     x+10,y+10,
                     fill='cyan',
                     width=0,tags='circle')
-    mx = x+30*math.cos(dir*12*math.pi/180)
-    my = y+30*math.sin(dir*12*math.pi/180)
-    cvs.create_oval(mx-2.5,my-2.5,
-                    mx+2.5,my+2.5,
-                    fill='white',
-                    width=0,tags='circle')
-    dir+=0.5
+    dir+=1
     root.after(17,main)
 
 root = tkinter.Tk()
@@ -27,7 +21,7 @@ cvs = tkinter.Canvas(root,width=500,
                      height=500,bg='black')
 cvs.pack()
 
-cvs.create_oval(230,230,270,270,
+cvs.create_oval(150,150,350,350,
                 fill='orange',width=0)
 main()
 root.mainloop()
---
+++ mathematics/round4_2.py
@@ -0,0 +1,35 @@
+import tkinter,math
+
+dir=0
+rw, rh = 200, 50 # rw:横半径、rh:縦半径
+centerX, centerY = 250, 250
+def main():
+    global dir
+    cvs.delete('circle')
+    x = centerX+rw*math.cos(dir*math.pi/180)
+    y = centerY+rh*math.sin(dir*math.pi/180)
+    cvs.create_arc(150,150,350,350,
+                   start=180,extent=180,
+                   fill='orange',
+                   outline='orange',
+                   width=0,tags='circle')
+    cvs.create_oval(x-10,y-10,
+                    x+10,y+10,
+                    fill='cyan',
+                    width=0,tags='circle')
+    cvs.create_arc(150,150,350,350,
+                   start=0,extent=180,
+                   fill='orange',
+                   outline='orange',
+                   width=0,tags='circle')
+    dir+=1
+    root.after(17,main)
+
+root = tkinter.Tk()
+root.title('地球')
+cvs = tkinter.Canvas(root,width=500,
+                     height=500,bg='black')
+cvs.pack()
+
+main()
+root.mainloop()
