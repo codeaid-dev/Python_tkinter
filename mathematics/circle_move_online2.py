238d592005c02d759a0c6d9a6d50b8f775418da7
codeaid-dev <codeaid.lab@gmail.com>
2026-04-27 20:48:34 +0900

    fix

--- mathematics/circle_move_online2.py
+++ mathematics/circle_move_online2.py
@@ -9,26 +9,27 @@ cvs.pack()
 x1,y1, = 100,100
 x2,y2 = 400,400
 circleX,circleY = x1,y1
-status = True
+target = (x2,y2)
+speed = 2
 def main():
-    global circleX,circleY,status
+    global circleX,circleY,target
     cvs.delete('circle')
-    if status:
-        dx = x2 - circleX
-        dy = y2 - circleY
-        dist = ((circleX - x2)**2+(circleY - y2)**2)**0.5
-        if dist < 1:
-            status = False
+    tx,ty = target
+    dx = tx - circleX
+    dy = ty - circleY
+    dist = (dx**2 + dy**2)**0.5
+    if dist < speed:
+        circleX, circleY = tx, ty
+        if target == (x2, y2):
+            target = (x1, y1)
+        else:
+            target = (x2, y2)
     else:
-        dx = x1 - circleX
-        dy = y1 - circleY
-        dist = ((circleX - x1)**2+(circleY - y1)**2)**0.5
-        if dist < 1:
-            status = True
-
-    angle = math.atan2(dy,dx)
-    circleX += math.cos(angle)*2
-    circleY += math.sin(angle)*2
+        angle = math.atan2(dy,dx)
+        circleX += math.cos(angle)*speed
+        circleY += math.sin(angle)*speed
+        # circleX += dx / dist * speed
+        # circleY += dy / dist * speed
 
     cvs.create_oval(circleX-30,circleY-30,
                     circleX+30,circleY+30,
